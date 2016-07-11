b7559132e4b8bf9b335a089e2556e788fc3ff188
Kate Walters <kawalters01@gmail.com>
2016-07-11 14:29:34 -0500

    Added routes and created controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
-class ProductsController > ApplicationController
+class ProductsController < ApplicationController
+
+  def index
+    render template: 'products/index.html.erb', locals: { product: Product.all }
+  end
+
+  def show
+    if Product.exists?(params[:id])
+      render template: 'products/show.html.erb', locals: { product: Product.find(params[:id])}
+    else
+      render html: "Product not found", status: 404
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'products', to: 'products#index'
   get 'products/:id', to: 'products#show'
 
-  root 'posts#index'
+  root 'products#index'
 end
