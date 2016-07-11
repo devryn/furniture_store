b0e0631cd4fa43dd6255514bdd71a383b64acd22
Kate Walters <kawalters01@gmail.com>
2016-07-11 14:22:06 -0500

    Generated Faker data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Product.create(name: Faker::Commerce.product_name, price: rand(1..5000), condition: ['excellent', 'good', 'average'].sample, on_clearance: rand(0..1), quantity: rand(0..5))
+end
