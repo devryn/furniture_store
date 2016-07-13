caee2b5e7e9e20fc24c8d626c92707383464b74b
Kate Walters <kawalters01@gmail.com>
2016-07-13 15:36:19 -0500

    Product show page formatted correctly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713183435) do
+ActiveRecord::Schema.define(version: 20160713200055) do
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160713183435) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.string   "photo_url"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
